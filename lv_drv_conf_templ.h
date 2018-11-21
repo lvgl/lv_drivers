b3435dd497a7a3e0f62b8d687485e8ce1a775999
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-11-21 12:05:13 +0100

    lv_drv_conf_templ.h: set MONITOR_ZOOM to 1

--- lv_drv_conf_templ.h
+++ lv_drv_conf_templ.h
@@ -86,7 +86,7 @@
 #if USE_MONITOR
 #define MONITOR_HOR_RES     LV_HOR_RES
 #define MONITOR_VER_RES     LV_VER_RES
-#define MONITOR_ZOOM        2                       /* Scale window by this factor (useful when simulating small screens) */
+#define MONITOR_ZOOM        1                       /* Scale window by this factor (useful when simulating small screens) */
 #define MONITOR_SDL_INCLUDE_PATH    <SDL2/SDL.h>    /*Eclipse: <SDL2/SDL.h>    Visual Studio: <SDL.h>*/
 #define MONITOR_VIRTUAL_MACHINE 0                   /*Different rendering should be used if running in a Virtual machine*/
 #endif
