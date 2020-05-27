478e176c1d788ad6264de3fc9944347dc5656c38
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-05-27 12:42:12 +0200

    simplify makefile

--- lv_drivers.mk
+++ lv_drivers.mk
@@ -1,10 +1,1 @@
-include $(LVGL_DIR)/lv_drivers/display/display.mk
-include $(LVGL_DIR)/lv_drivers/indev/indev.mk
-
-
-CSRCS += win_drv.c
-
-DEPPATH += --dep-path $(LVGL_DIR)/lv_drivers
-VPATH += :$(LVGL_DIR)/lv_drivers
-
-CFLAGS += "-I$(LVGL_DIR)/lv_drivers"
+CSRCS += $(shell find -L lv_drivers -name \*.c)
