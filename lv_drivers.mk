233860e778b37fa6b6a7f6221741d899617b796e
Sven Krauß <sven.krauss@embedded-solution.de>
2020-07-28 17:11:35 +0200

    Use wildcard function instead of find.
    This fix uses the wildcard function instead of find because it is
    possible to use other subdirectories for building.

--- lv_drivers.mk
+++ lv_drivers.mk
@@ -1,1 +1,7 @@
-CSRCS += $(shell find -L lv_drivers -name \*.c)
+LV_DRIVERS_DIR_NAME ?= lv_drivers
+
+CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/*.c)
+CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/indev/*.c)
+CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/gtk/*.c)
+CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/display/*.c)
+
