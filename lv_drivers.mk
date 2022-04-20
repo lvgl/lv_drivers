6aa134b4c44e45c01e1579764deaffa94497f59b
Ayoub Zaki <ayoub.zaki@embexus.com>
2022-04-20 14:45:45 +0200

    fix Makefile for some cross compilers

--- lv_drivers.mk
+++ lv_drivers.mk
@@ -1,5 +1,7 @@
 LV_DRIVERS_DIR_NAME ?= lv_drivers
 
+override CFLAGS := -I$(LVGL_DIR) $(CFLAGS)
+
 CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/*.c)
 CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/wayland/*.c)
 CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/indev/*.c)
