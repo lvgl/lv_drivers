bd7752d938b0fa38d680fe257ebe194431c5ac76
Niklas Fiekas <niklas.fiekas@backscattering.de>
2023-10-21 11:02:31 +0200

    feat(evdev): backport multi device support from lvgl 9

--- indev/evdev.c
+++ indev/evdev.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 #if USE_BSD_EVDEV
 #include <dev/evdev/input.h>
 #else
@@ -22,233 +23,202 @@
 #include "xkb.h"
 #endif /* USE_XKB */
 
-/*********************
- *      DEFINES
- *********************/
-
-/**********************
- *      TYPEDEFS
- **********************/
-
-/**********************
- *  STATIC PROTOTYPES
- **********************/
-int map(int x, int in_min, int in_max, int out_min, int out_max);
-
 /**********************
  *  STATIC VARIABLES
  **********************/
-int evdev_fd = -1;
-int evdev_root_x;
-int evdev_root_y;
-int evdev_button;
 
-int evdev_key_val;
+evdev_device_t global_dsc;
 
 /**********************
- *      MACROS
+ *  STATIC FUNCTIONS
  **********************/
+
+static int _evdev_process_key(uint16_t code, bool pressed)
+{
+#if USE_XKB
+    return xkb_process_key(code, pressed);
+#else
+    LV_UNUSED(pressed);
+    switch(code) {
+        case KEY_UP:
+            return LV_KEY_UP;
+        case KEY_DOWN:
+            return LV_KEY_DOWN;
+        case KEY_RIGHT:
+            return LV_KEY_RIGHT;
+        case KEY_LEFT:
+            return LV_KEY_LEFT;
+        case KEY_ESC:
+            return LV_KEY_ESC;
+        case KEY_DELETE:
+            return LV_KEY_DEL;
+        case KEY_BACKSPACE:
+            return LV_KEY_BACKSPACE;
+        case KEY_ENTER:
+            return LV_KEY_ENTER;
+        case KEY_NEXT:
+        case KEY_TAB:
+            return LV_KEY_NEXT;
+        case KEY_PREVIOUS:
+            return LV_KEY_PREV;
+        case KEY_HOME:
+            return LV_KEY_HOME;
+        case KEY_END:
+            return LV_KEY_END;
+        default:
+            return 0;
+    }
+#endif /*USE_XKB*/
+}
+
+static int _evdev_calibrate(int v, int in_min, int in_max, int out_min, int out_max)
+{
+    if(in_min != in_max) v = (v - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return LV_CLAMP(out_min, v, out_max);
+}
+
+static lv_point_t _evdev_process_pointer(lv_indev_drv_t * drv, int x, int y)
+{
+    evdev_device_t * dsc = drv->user_data ? drv->user_data : &global_dsc;
+
+    int swapped_x = dsc->swap_axes ? y : x;
+    int swapped_y = dsc->swap_axes ? x : y;
+
+    int offset_x = 0; /*Not lv_disp_get_offset_x(drv->disp) for bc*/
+    int offset_y = 0; /*Not lv_disp_get_offset_y(drv->disp) for bc*/
+    int width = lv_disp_get_hor_res(drv->disp);
+    int height = lv_disp_get_ver_res(drv->disp);
+
+    lv_point_t p;
+    p.x = _evdev_calibrate(swapped_x, dsc->hor_min, dsc->hor_max, offset_x, offset_x + width - 1);
+    p.y = _evdev_calibrate(swapped_y, dsc->ver_min, dsc->ver_max, offset_y, offset_y + height - 1);
+    return p;
+}
 
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
 
-/**
- * Initialize the evdev interface
- */
-void evdev_init(void)
+void evdev_init()
 {
-    if (!evdev_set_file(EVDEV_NAME)) {
-        return;
-    }
+    evdev_device_init(&global_dsc);
+#ifdef EVDEV_SWAP_AXES
+    evdev_device_set_swap_axes(&global_dsc, EVDEV_SWAP_AXES);
+#endif
+#if EVDEV_CALIBRATE
+    evdev_device_set_calibration(&global_dsc, EVDEV_HOR_MIN, EVDEV_VER_MIN, EVDEV_HOR_MAX, EVDEV_VER_MAX);
+#endif
+    evdev_device_set_file(&global_dsc, EVDEV_NAME);
+}
+
+void evdev_device_init(evdev_device_t * dsc)
+{
+    lv_memset(dsc, 0, sizeof(evdev_device_t));
+    dsc->fd = -1;
 
 #if USE_XKB
     xkb_init();
 #endif
 }
-/**
- * reconfigure the device file for evdev
- * @param dev_name set the evdev device filename
- * @return true: the device file set complete
- *         false: the device file doesn't exist current system
- */
-bool evdev_set_file(char* dev_name)
-{ 
-     if(evdev_fd != -1) {
-        close(evdev_fd);
-     }
-#if USE_BSD_EVDEV
-     evdev_fd = open(dev_name, O_RDWR | O_NOCTTY);
-#else
-     evdev_fd = open(dev_name, O_RDWR | O_NOCTTY | O_NDELAY);
-#endif
 
-     if(evdev_fd == -1) {
-        perror("unable to open evdev interface:");
-        return false;
-     }
-
-#if USE_BSD_EVDEV
-     fcntl(evdev_fd, F_SETFL, O_NONBLOCK);
-#else
-     fcntl(evdev_fd, F_SETFL, O_ASYNC | O_NONBLOCK);
-#endif
-
-     evdev_root_x = 0;
-     evdev_root_y = 0;
-     evdev_key_val = 0;
-     evdev_button = LV_INDEV_STATE_REL;
-
-     return true;
+bool evdev_set_file(const char * dev_path)
+{
+    return evdev_device_set_file(&global_dsc, dev_path);
 }
-/**
- * Get the current position and state of the evdev
- * @param data store the evdev data here
- */
+
+bool evdev_device_set_file(evdev_device_t * dsc, const char * dev_path)
+{
+    /*Reset state*/
+    dsc->root_x = 0;
+    dsc->root_y = 0;
+    dsc->key = 0;
+    dsc->state = LV_INDEV_STATE_RELEASED;
+
+    /*Close previous*/
+    if(dsc->fd >= 0) {
+        close(dsc->fd);
+        dsc->fd = -1;
+    }
+    if(!dev_path) return false;
+
+    /*Open new*/
+    dsc->fd = open(dev_path, O_RDONLY | O_NOCTTY | O_CLOEXEC);
+    if(dsc->fd < 0) {
+        LV_LOG_ERROR("open failed: %s", strerror(errno));
+        return false;
+    }
+    if(fcntl(dsc->fd, F_SETFL, O_NONBLOCK) < 0) {
+        LV_LOG_ERROR("fcntl failed: %s", strerror(errno));
+        close(dsc->fd);
+        dsc->fd = -1;
+        return false;
+    }
+
+    return true;
+}
+
+void evdev_device_set_swap_axes(evdev_device_t * dsc, bool swap_axes)
+{
+    dsc->swap_axes = swap_axes;
+}
+
+void evdev_device_set_calibration(evdev_device_t * dsc, int ver_min, int hor_min, int ver_max, int hor_max)
+{
+    dsc->ver_min = ver_min;
+    dsc->hor_min = hor_min;
+    dsc->ver_max = ver_max;
+    dsc->hor_max = hor_max;
+}
+
 void evdev_read(lv_indev_drv_t * drv, lv_indev_data_t * data)
 {
-    struct input_event in;
+    evdev_device_t * dsc = drv->user_data ? drv->user_data : &global_dsc;
+    if(dsc->fd < 0) return;
 
-    while(read(evdev_fd, &in, sizeof(struct input_event)) > 0) {
+    /*Update dsc with buffered events*/
+    struct input_event in = { 0 };
+    while(read(dsc->fd, &in, sizeof(in)) > 0) {
         if(in.type == EV_REL) {
-            if(in.code == REL_X)
-				#if EVDEV_SWAP_AXES
-					evdev_root_y += in.value;
-				#else
-					evdev_root_x += in.value;
-				#endif
-            else if(in.code == REL_Y)
-				#if EVDEV_SWAP_AXES
-					evdev_root_x += in.value;
-				#else
-					evdev_root_y += in.value;
-				#endif
-        } else if(in.type == EV_ABS) {
-            if(in.code == ABS_X)
-				#if EVDEV_SWAP_AXES
-					evdev_root_y = in.value;
-				#else
-					evdev_root_x = in.value;
-				#endif
-            else if(in.code == ABS_Y)
-				#if EVDEV_SWAP_AXES
-					evdev_root_x = in.value;
-				#else
-					evdev_root_y = in.value;
-				#endif
-            else if(in.code == ABS_MT_POSITION_X)
-                                #if EVDEV_SWAP_AXES
-                                        evdev_root_y = in.value;
-                                #else
-                                        evdev_root_x = in.value;
-                                #endif
-            else if(in.code == ABS_MT_POSITION_Y)
-                                #if EVDEV_SWAP_AXES
-                                        evdev_root_x = in.value;
-                                #else
-                                        evdev_root_y = in.value;
-                                #endif
+            if(in.code == REL_X) dsc->root_x += in.value;
+            else if(in.code == REL_Y) dsc->root_y += in.value;
+        }
+        else if(in.type == EV_ABS) {
+            if(in.code == ABS_X || in.code == ABS_MT_POSITION_X) dsc->root_x = in.value;
+            else if(in.code == ABS_Y || in.code == ABS_MT_POSITION_Y) dsc->root_y = in.value;
             else if(in.code == ABS_MT_TRACKING_ID) {
-                                if(in.value == -1)
-                                    evdev_button = LV_INDEV_STATE_REL;
-                                else if(in.value == 0)
-                                    evdev_button = LV_INDEV_STATE_PR;
+                if(in.value == -1) dsc->state = LV_INDEV_STATE_RELEASED;
+                else if(in.value == 0) dsc->state = LV_INDEV_STATE_PRESSED;
             }
-        } else if(in.type == EV_KEY) {
+        }
+        else if(in.type == EV_KEY) {
             if(in.code == BTN_MOUSE || in.code == BTN_TOUCH) {
-                if(in.value == 0)
-                    evdev_button = LV_INDEV_STATE_REL;
-                else if(in.value == 1)
-                    evdev_button = LV_INDEV_STATE_PR;
-            } else if(drv->type == LV_INDEV_TYPE_KEYPAD) {
-#if USE_XKB
-                data->key = xkb_process_key(in.code, in.value != 0);
-#else
-                switch(in.code) {
-                    case KEY_BACKSPACE:
-                        data->key = LV_KEY_BACKSPACE;
-                        break;
-                    case KEY_ENTER:
-                        data->key = LV_KEY_ENTER;
-                        break;
-                    case KEY_PREVIOUS:
-                        data->key = LV_KEY_PREV;
-                        break;
-                    case KEY_NEXT:
-                        data->key = LV_KEY_NEXT;
-                        break;
-                    case KEY_UP:
-                        data->key = LV_KEY_UP;
-                        break;
-                    case KEY_LEFT:
-                        data->key = LV_KEY_LEFT;
-                        break;
-                    case KEY_RIGHT:
-                        data->key = LV_KEY_RIGHT;
-                        break;
-                    case KEY_DOWN:
-                        data->key = LV_KEY_DOWN;
-                        break;
-                    case KEY_TAB:
-                        data->key = LV_KEY_NEXT;
-                        break;
-                    default:
-                        data->key = 0;
-                        break;
+                if(in.value == 0) dsc->state = LV_INDEV_STATE_RELEASED;
+                else if(in.value == 1) dsc->state = LV_INDEV_STATE_PRESSED;
+            }
+            else {
+                dsc->key = _evdev_process_key(in.code, in.value != 0);
+                if(dsc->key) {
+                    dsc->state = in.value ? LV_INDEV_STATE_PRESSED : LV_INDEV_STATE_RELEASED;
+                    data->continue_reading = true; /*Keep following events in buffer for now*/
+                    break;
                 }
-#endif /* USE_XKB */
-                if (data->key != 0) {
-                    /* Only record button state when actual output is produced to prevent widgets from refreshing */
-                    data->state = (in.value) ? LV_INDEV_STATE_PR : LV_INDEV_STATE_REL;
-                }
-                evdev_key_val = data->key;
-                evdev_button = data->state;
-                return;
             }
         }
     }
 
-    if(drv->type == LV_INDEV_TYPE_KEYPAD) {
-        /* No data retrieved */
-        data->key = evdev_key_val;
-        data->state = evdev_button;
-        return;
+    /*Process and store in data*/
+    switch(drv->type) {
+        case LV_INDEV_TYPE_KEYPAD:
+            data->state = dsc->state;
+            data->key = dsc->key;
+            break;
+        case LV_INDEV_TYPE_POINTER:
+            data->state = dsc->state;
+            data->point = _evdev_process_pointer(drv, dsc->root_x, dsc->root_y);
+            break;
+        default:
+            break;
     }
-    if(drv->type != LV_INDEV_TYPE_POINTER)
-        return ;
-    /*Store the collected data*/
-
-#if EVDEV_CALIBRATE
-    data->point.x = map(evdev_root_x, EVDEV_HOR_MIN, EVDEV_HOR_MAX, 0, drv->disp->driver->hor_res);
-    data->point.y = map(evdev_root_y, EVDEV_VER_MIN, EVDEV_VER_MAX, 0, drv->disp->driver->ver_res);
-#else
-    evdev_root_x = LV_CLAMP(0, evdev_root_x, drv->disp->driver->hor_res-1);
-    evdev_root_y = LV_CLAMP(0, evdev_root_y, drv->disp->driver->ver_res-1);
-
-    data->point.x = evdev_root_x;
-    data->point.y = evdev_root_y;
-#endif
-
-    data->state = evdev_button;
-
-    if(data->point.x < 0)
-      data->point.x = 0;
-    if(data->point.y < 0)
-      data->point.y = 0;
-    if(data->point.x >= drv->disp->driver->hor_res)
-      data->point.x = drv->disp->driver->hor_res - 1;
-    if(data->point.y >= drv->disp->driver->ver_res)
-      data->point.y = drv->disp->driver->ver_res - 1;
-
-    return ;
 }
 
-/**********************
- *   STATIC FUNCTIONS
- **********************/
-int map(int x, int in_min, int in_max, int out_min, int out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-#endif
+#endif /*USE_EVDEV*/
--- indev/evdev.h
+++ indev/evdev.h
@@ -29,39 +29,81 @@ extern "C" {
 #include "lvgl/lvgl.h"
 #endif
 
-/*********************
- *      DEFINES
- *********************/
-
 /**********************
  *      TYPEDEFS
  **********************/
+
+typedef struct {
+    /*Device*/
+    int fd;
+    /*Config*/
+    bool swap_axes;
+    int ver_min;
+    int hor_min;
+    int ver_max;
+    int hor_max;
+    /*State*/
+    int root_x;
+    int root_y;
+    int key;
+    lv_indev_state_t state;
+} evdev_device_t;
 
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
 
 /**
- * Initialize the evdev
+ * Initialize the global evdev device, as configured with EVDEV_NAME,
+ * EVDEV_SWAP_AXES, and EVDEV_CALIBRATE.
  */
-void evdev_init(void);
+void evdev_init();
 /**
- * reconfigure the device file for evdev
- * @param dev_name set the evdev device filename
- * @return true: the device file set complete
- *         false: the device file doesn't exist current system
+ * Initialize an evdev device.
+ * @param dsc evdev device
  */
-bool evdev_set_file(char* dev_name);
+void evdev_device_init(evdev_device_t * dsc);
+
 /**
- * Get the current position and state of the evdev
- * @param data store the evdev data here
+ * Reconfigure the path for the global evdev device.
+ * @param dev_path device path, e.g., /dev/input/event0, or NULL to close
+ * @return whether the device was successfully opened
+ */
+bool evdev_set_file(const char * dev_path);
+/**
+ * Configure or reconfigure the path for an evdev device.
+ * @param dsc evdev device
+ * @param dev_path device path, e.g., /dev/input/event0, or NULL to close
+ * @return whether the device was successfully opened
+ */
+bool evdev_device_set_file(evdev_device_t * dsc, const char * dev_path);
+
+/**
+ * Configure whether pointer coordinates of an evdev device sould be swapped.
+ * Default to false.
+ * @param dsc evdev device
+ * @param swap_axes whether to swap x and y axes
+ */
+void evdev_device_set_swap_axes(evdev_device_t * dsc, bool swap_axes);
+
+/**
+ * Configure a coordinate transformation for an evdev device. Applied after
+ * axis swap, if any. Defaults to no transformation.
+ * @param dsc evdev device
+ * @param hor_min horizontal pointer coordinate mapped to 0
+ * @param ver_min vertical pointer coordinate mapped to 0
+ * @param ver_min pointer coordinate mapped to horizontal max of display
+ * @param ver_max pointer coordinate mapped to vertical max of display
+ */
+void evdev_device_set_calibration(evdev_device_t * dsc, int hor_min, int ver_min, int hor_max, int ver_max);
+
+/**
+ * Read callback for the input driver.
+ * @param drv input driver where drv->user_data is NULL for the global evdev
+ *            device or an evdev_device_t pointer.
+ * @param data destination for input events
  */
 void evdev_read(lv_indev_drv_t * drv, lv_indev_data_t * data);
-
-
-/**********************
- *      MACROS
- **********************/
 
 #endif /* USE_EVDEV */
 
